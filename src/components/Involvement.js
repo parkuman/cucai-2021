3e190a2df4394d833d020b58656a45a57a85a7a0
Katie Lu <katielu@Katies-MacBook-Pro.local>
2020-10-04 17:58:47 -0400

    Added PDFs for involvement

--- src/components/Involvement.js
+++ src/components/Involvement.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Buttom from "../components/Button";
+import Button from "../components/Button";
 import styled from "styled-components";
 
 import { Tab, Row, Col, Nav } from 'react-bootstrap/';
