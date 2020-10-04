ad858df68689045c102b46d8fe69eca514c372d6
Katie Lu <katielu@Katies-MacBook-Pro.local>
2020-10-04 17:57:46 -0400

    Added PDFs for involvement

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {Card} from 'react-bootstrap'
+import {Card, CardDeck, Carousel} from 'react-bootstrap'
 
 const StyledCardWrapper = styled.div`
 position: relative;
@@ -30,38 +30,62 @@ position: relative;
   `;
 
   const CardStack = () => {
+    return(
+      <Carousel>
+
+      <Carousel.Item>
+      <CardDeck>
+
+      <Card>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>Geoffrey Hinton</Card.Title>
+        <Card.Text>World renowned researcher Geoffrey Hinton, winner of the 2018 ACM A. M. Turing Award (jointly with Yoshua Bengio and Yann LeCun), discussed his most recent research efforts and gave advice to aspiring students. In a technical presentation, Dr. Hinton described his work on model distillation, a process by which the knowledge contained within larger, more complex ensembles of models can be transferred to those which are smaller and require less computation. Dr. Hinton showed that by softening the large models’ outputs, a great deal of information can be extracted from its mistakes.
+        </Card.Text>
+      </Card.Body>
+      </Card>
+
+      <Card>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>Women in AI</Card.Title>
+        <Card.Text>Tania De Gasperis from Over Under Inside Out, Jenny Lemieux from Torstar, and Dr. Amy Wu from Queen’s University discussed using AI as a tool for change. The panel was moderated by our very own, Sam Dunn, a 4th year Mechanical Engineering student and QMIND’s Director of Marketing. The panel discussion revolved around the changing industries of journalism, start ups, and biomechanics, in addition to why these women were driven to pursue careers in STEM despite the societal barriers that they encounter each and every day.
+     </Card.Text>
+      </Card.Body>
+      </Card>
+
+      <Card>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>NLP x Game of Thrones</Card.Title>
+        <Card.Text>
+        Alexander Scott, an Analytics Instructor at the Smith School of Business, provided an interactive workshop for delegates on natural language processing (NLP) and its importance in various industries. He went through the basic tasks required for NLP projects and explained their importance. The delegates were given the entire script of Game of Thrones as textual data to apply these techniques. The goal was to retrieve all of the lines associated with a specific character from the show. The process involved pulling the lines from a single character, cleaning the text, visualizing the words through a “word cloud” diagram, and determining the importance of each word through text vectorization. The final result was the lines for a single character, the number of lines, and the vectorization of each word.
+        </Card.Text>
+      </Card.Body>
+      </Card>
+</CardDeck>
+      </Carousel.Item>
+
+      <Carousel.Item>
+      <Card>
+      </Card>
+      <Card>
+      </Card>
+      <Card>
+      </Card>
+      </Carousel.Item>
+
+      </Carousel>
+
+    );
+  }
+
+  const Cards = () => {
       return(
           <StyledCardWrapper>
 
-            <StyledCard>
-  <Card.Img variant="top" src="" />
-  <Card.Body>
-    <Card.Title>Geoffrey Hinton</Card.Title>
-    <Card.Text>World renowned researcher Geoffrey Hinton, winner of the 2018 ACM A. M. Turing Award (jointly with Yoshua Bengio and Yann LeCun), discussed his most recent research efforts and gave advice to aspiring students. In a technical presentation, Dr. Hinton described his work on model distillation, a process by which the knowledge contained within larger, more complex ensembles of models can be transferred to those which are smaller and require less computation. Dr. Hinton showed that by softening the large models’ outputs, a great deal of information can be extracted from its mistakes.
-    </Card.Text>
-  </Card.Body>
-</StyledCard>
-
-            <StyledCard>
-  <Card.Img variant="top" src="" />
-  <Card.Body>
-    <Card.Title>NLP x Game of Thrones</Card.Title>
-    <Card.Text>
-    Alexander Scott, an Analytics Instructor at the Smith School of Business, provided an interactive workshop for delegates on natural language processing (NLP) and its importance in various industries. He went through the basic tasks required for NLP projects and explained their importance. The delegates were given the entire script of Game of Thrones as textual data to apply these techniques. The goal was to retrieve all of the lines associated with a specific character from the show. The process involved pulling the lines from a single character, cleaning the text, visualizing the words through a “word cloud” diagram, and determining the importance of each word through text vectorization. The final result was the lines for a single character, the number of lines, and the vectorization of each word.
-    </Card.Text>
-  </Card.Body>
-</StyledCard>
-
-<StyledCard>
-  <Card.Img variant="top" src="" />
-  <Card.Body>
-    <Card.Title>Women in AI</Card.Title>
-    <Card.Text>Tania De Gasperis from Over Under Inside Out, Jenny Lemieux from Torstar, and Dr. Amy Wu from Queen’s University discussed using AI as a tool for change. The panel was moderated by our very own, Sam Dunn, a 4th year Mechanical Engineering student and QMIND’s Director of Marketing. The panel discussion revolved around the changing industries of journalism, start ups, and biomechanics, in addition to why these women were driven to pursue careers in STEM despite the societal barriers that they encounter each and every day.
- </Card.Text>
-  </Card.Body>
-</StyledCard>
           </StyledCardWrapper>
-        
+
       );
   }
 
